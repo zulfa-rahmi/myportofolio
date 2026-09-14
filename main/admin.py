8830b7de2ac0c4e5340d655fd6cee8752b77f777
zulfa-rahmi <zrahminasution@gmail.com>
2026-09-14 21:03:26 +0700

    feat: menambahkan fitur projects, dan tampilan awal website

--- main/admin.py
+++ main/admin.py
@@ -1,13 +1,1 @@
 from django.contrib import admin
-
-@admin.register(Experience)
-class ExperienceAdmin(admin.ModelAdmin):
-    list_display = ("title", "organization", "category", "started_at", "ended_at")
-    list_filter = ("category",)
-    search_fields = ("title", "organization", "description")
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ("title", "event", "year")
-    search_fields = ("title", "event", "description")
-    ordering = ("-year",)
